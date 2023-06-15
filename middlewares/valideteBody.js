2e2bdfd70a29d155289254ebfaced290f27216d8
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-06-15 15:12:36 +0300

    add mongoose methods for requests

--- middlewares/valideteBody.js
+++ middlewares/valideteBody.js
@@ -1,7 +1,11 @@
 const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => async (req, res, next) => {
+  const { path } = req.route;
   const { error } = schema.validate(req.body);
+  if (error && path === "/:contactId/favorite") {
+    next(HttpError(400, "Missing field favorite"));
+  }
   if (error) {
     next(HttpError(400, "Missing required name field"));
   }
